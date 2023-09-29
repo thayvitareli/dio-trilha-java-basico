b990d8df0c46374d7b091dead3ec7c53554a501e
= <thainyvitareli@hotmail.com>
2023-09-28 21:24:07 -0300

    implementacao do código de desafio

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+      
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = sc.nextLine();
+        System.out.println("numero da conta:");
+        Integer numero = sc.nextInt();
+        System.out.println("Por favor, digite o nome: ");
+        sc.nextLine();
+        String nome = sc.nextLine();
+        System.out.println("Digite o Saldo:");
+        Double saldo = sc.nextDouble();
+       
+        System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo +" já está disponível para saque");
+    
+
+        sc.close();
+    }   
 }
